ae46fc48cd605606a4fb9b289b8cd4f88689e946
Amadeus <38767929+Mozart409@users.noreply.github.com>
2020-08-30 13:41:34 +0200

    added e2e tests

---
+++ cypress/integration/home_layout_spec.js
@@ -0,0 +1,5 @@
+describe('Loads the Homepage and test the layout component', () => {
+  it('successfully loads layout', () => {
+    cy.visit('/')
+  })
+})
--- cypress/integration/home_loading_all_packages.js
+++ cypress/integration/home_loading_all_packages.js
@@ -1,7 +1,8 @@
 describe('Fetch all Packages', () => {
   it('successfully fetches n+1 package/s', () => {
     cy.visit('/')
-    cy.get('.SinglePackage')
-    cy.get('.AllPackages')
+    cy.contains('Browse all packages')
+    cy.get('.AllPackages').find('.SinglePackage').its('length').should('be.gte', 1)
+    cy.get('.AllPackages').should('have.length', 1)
   })
 })
